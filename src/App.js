d5d7b6b6d8ef0f35f2d196516f0128c310928327
hatef <hatef.khodkar@hotmail.com>
2022-09-15 11:57:42 +0430

    add navigation bar to project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import Categories from './components/categories/Categories';
+import { Routes, Route } from 'react-router-dom'
+
+import NavigationBar from './pages/navigation/NavigationBar';
+
+import Home from "./pages/home/Home";
+
+const Shop = () => {
+  return <h1>shop</h1>
+}
 
 const App = () => {
   return (
-    <Categories />
-  );
+    <Routes>
+      <Route path='/' element={<NavigationBar />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App;
+
---
+++ src/pages/navigation/NavigationBar.js
@@ -0,0 +1,25 @@
+import { Fragment } from "react"
+import { Outlet, Link } from "react-router-dom"
+import classes from './NavigationBar.module.scss'
+import { ReactComponent as Logo } from '../../assets/crown.svg'
+
+
+const NavigationBar = props => {
+    return (
+        <Fragment>
+            <div className={classes.navigation}>
+                <div className={classes['logo-container']} >
+                    <Link to="/">
+                        <Logo className={classes.logo} />
+                    </Link>
+                </div>
+                <div className={classes['nav-links-container']}>
+                    <Link className={classes['nav-link']} to='/shop'>Shop</Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    )
+}
+
+export default NavigationBar
