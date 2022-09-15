931d4376c40f1e5ba82d946bbb2089791094c500
hatef <hatef.khodkar@hotmail.com>
2022-09-15 11:57:30 +0430

    add home component

---
+++ src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react"
+
+import Categories from "../../components/categories/Categories"
+
+
+const Home = props => {
+    return (
+        <Fragment>
+            <Categories />
+        </Fragment>
+    )
+}
+
+export default Home
