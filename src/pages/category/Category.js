240f4718492162d2aae62be1c386a4a66a174002
hatef <hatef.khodkar@hotmail.com>
2022-09-22 19:33:22 +0330

    add category page

---
+++ src/pages/category/Category.js
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import ProductCard from '../../components/product-card/ProductCard';
+
+import { CategoriesContext } from '../../contexts/CategorisContext'
+
+import classes from './Category.module.scss'
+
+const Category = props => {
+    const { category } = useParams();
+    const { categoriesMap } = useContext(CategoriesContext);
+    const [products, setProducts] = useState(categoriesMap[category])
+
+    useEffect(() => {
+        setProducts(categoriesMap[category])
+    }, [category, categoriesMap])
+
+    return (
+        <div className={classes['category-container']}>
+            {
+                products &&
+                products.map((product) => <ProductCard key={product.id} product={product} />)
+            }
+        </div>
+    )
+}
+
+export default Category;
--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 
+import Category from '../category/Category';
 import CategoriesPreview from '../categories-preview/CategoriesPreview';
 
 const Shop = props => {
     return (
         <Routes>
             <Route index element={<CategoriesPreview />} />
+            <Route path=':category' element={<Category />} />
         </Routes>
     )
 }
